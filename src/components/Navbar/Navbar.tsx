fea9635bad25bd916c25293d0f975f1d3fce96db
Antonio Galindo <galindosvq@gmail.com>
2022-04-03 21:02:33 +0200

    feat(App): create mobile menu

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import { menuRoutes } from 'components/Menu/Menu';
+import { NavigationBar, NavLink } from './Navbar.style';
+
+const Navbar = () => (
+	<NavigationBar>
+		<ul>
+			{menuRoutes.map((route) => (
+				<li key={route.path}>
+					<NavLink
+						to={route.path}
+						title={route.title}
+						target={route.target}
+						rel={route.rel}
+					>
+						{route.icon}
+						{route.title}
+					</NavLink>
+				</li>
+			))}
+		</ul>
+	</NavigationBar>
+);
+
+export default Navbar;
