f27cc636431e3363de29a9825176b8b82b757ffa
Antonio Galindo <galindosvq@gmail.com>
2022-04-05 08:37:46 +0200

    feat(Footer): add data

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,26 @@ import React from 'react';
 import { Container } from './Footer.style';
 
 const Footer = () => {
-	return <Container>FOOTER</Container>;
+	return (
+		<Container>
+			<p>
+				Handmade 🤟
+				<a href="http://galindosvq.com" target="_blank" rel="noreferrer">
+					{' '}
+					Antonio Galindo
+				</a>{' '}
+				|
+				<a
+					href="https://github.com/galindosvq/imperial-destroyers/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					{' '}
+					Code
+				</a>
+			</p>
+		</Container>
+	);
 };
 
 export { Footer };
