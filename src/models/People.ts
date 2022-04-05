5f5ef249f77107e9cd074248890c71b3e0e2b73f
Antonio Galindo <galindosvq@gmail.com>
2022-04-05 06:59:00 +0200

    chore: create models

---
+++ src/models/People.ts
@@ -0,0 +1,19 @@
+export default interface People {
+	name: string;
+	birth_year: string;
+	eye_color: string;
+	gender: string;
+	hair_color: string;
+	height: string;
+	mass: string;
+	skin_color: string;
+	homeworld: string;
+	residents: string[];
+	films: string[];
+	species: string[];
+	starships: string[];
+	vehicles: string[];
+	url: string;
+	created: string;
+	edited: string;
+}
---
+++ src/models/Starship.ts
@@ -0,0 +1,27 @@
+export default interface Starship {
+	name: string;
+	model: string;
+	starship_class: string;
+	manufacturer: string;
+	cost_in_credits: string;
+	length: string;
+	crew: string;
+	passengers: string;
+	max_atmosphering_speed: string;
+	hyperdrive_rating: string;
+	MGLT: string;
+	cargo_capacity: string;
+	consumables: string;
+	films: string[];
+	pilots: string[];
+	url: string;
+	created: string;
+	edited: string;
+}
+
+export type StarshipResponse = {
+	count: number;
+	next: string | null;
+	previous: string | null;
+	results: Starship[];
+};
